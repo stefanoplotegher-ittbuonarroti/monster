55d673f15553ba5028516122814adef68e3b94f0
STEFANO.PLOTEGHER <STEFANO.PLOTEGHER@buonarroti.tn.it>
2022-11-09 16:11:28 +0100

    feat: aggiunto il costruttore

--- src/com/company/Monster.java
+++ src/com/company/Monster.java
@@ -4,5 +4,11 @@ public class Monster {
     private String nome;
     private int puntiVita;
     private int danno;
-    
+
+
+    public Monster (String nome, int puntiVita, int danno){
+        this.nome=nome;
+        this.puntiVita=puntiVita;
+        this.danno=danno;
+    }
 }
