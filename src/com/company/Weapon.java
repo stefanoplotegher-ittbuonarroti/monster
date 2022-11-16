b2f706faa29fac6293a58059248386a996a9210e
STEFANO.PLOTEGHER <STEFANO.PLOTEGHER@buonarroti.tn.it>
2022-11-16 16:02:04 +0100

    feat: aggiunti attributi e costruttore

--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class Weapon {
+    private int danni;
+    private String nomeArma;
+
+    public Weapon (int danni, String nomeArma){
+        this.danni=danni;
+        this.nomeArma=nomeArma;
+    }
 }
