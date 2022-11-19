d33b5aaf66c984db9b3ceab836ac54ed3d6dd67a
STEFANO.PLOTEGHER <stefano.plotegher@buonarroti.tn.it>
2022-11-19 10:04:12 +0100

    feat: aggiunti due variabili nel main per definire le armi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,13 @@ public class Main {
         int i = 0;
         Random rand = new Random();
         int attacco = 0;
+        String [] arrayDeiNomi={"la potente Excalibur","un M16","la Daga del Brigante", "la un po' meno potente Excalibur","l'Arco dei Mille Sospiri"," Primiera e Ori","la Frusta della Giustizia","il Fioretto di Fuoco","la un po' più potente Excalibur","la Bolletta del Gas"}
+        int [] arrayDeiDanni={50,10,23,15,28,41,30,45,67,90};
 
         gargoyle = new Monster("Marco il Gargoyle",500);
         yeti = new Monster("Enrico lo Yeti", 500);
+
+
 
         while(yeti.getPuntiVita()>0 && gargoyle.getPuntiVita()>0){
             i = rand.nextInt(2);
