88ccf0735a6be0eac434e4f78f30cb641418b2a1
STEFANO.PLOTEGHER <stefano.plotegher@buonarroti.tn.it>
2022-11-19 10:12:43 +0100

    feat: aggiunta nel main la possibilità di raccogliere o perdere l'arma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Monster gargoyle;
         Monster yeti;
-        int i = 0;
+        int i;
+        int k;
         Random rand = new Random();
         int attacco = 0;
-        String [] arrayDeiNomi={"la potente Excalibur","un M16","la Daga del Brigante", "la un po' meno potente Excalibur","l'Arco dei Mille Sospiri"," Primiera e Ori","la Frusta della Giustizia","il Fioretto di Fuoco","la un po' più potente Excalibur","la Bolletta del Gas"}
+        String [] arrayDeiNomi={"la potente Excalibur","un M16","la Daga del Brigante", "la un po' meno potente Excalibur","l'Arco dei Mille Sospiri"," Primiera e Ori","la Frusta della Giustizia","il Fioretto di Fuoco","la un po' più potente Excalibur","la Bolletta del Gas"};
         int [] arrayDeiDanni={50,10,23,15,28,41,30,45,67,90};
 
         gargoyle = new Monster("Marco il Gargoyle",500);
@@ -18,16 +19,40 @@ public class Main {
 
 
         while(yeti.getPuntiVita()>0 && gargoyle.getPuntiVita()>0){
-            i = rand.nextInt(2);
+            i = rand.nextInt(4);
             switch (i){
                 case 0:
                     attacco= rand.nextInt(3);
                     yeti.subisci(gargoyle.attacco(attacco));
-                    System.out.println("\n- "gargoyle.getNome()+" "+gargoyle.getMossa(attacco)+yeti.getNome()+" togliendoli "+gargoyle.attacco(attacco)+" punti vita.");
+                    System.out.println("\n- "+gargoyle.getNome()+" "+gargoyle.getMossa(attacco)+yeti.getNome()+" togliendoli "+gargoyle.attacco(attacco)+" punti vita.");
                     break;
                 case 1:
                     gargoyle.subisci(yeti.attacco(attacco));
-                    System.out.println("\n- "yeti.getNome()+" "+yeti.getMossa(attacco)+gargoyle.getNome()+" togliendoli "+yeti.attacco(attacco)+" punti vita.");
+                    System.out.println("\n- "+yeti.getNome()+" "+yeti.getMossa(attacco)+gargoyle.getNome()+" togliendoli "+yeti.attacco(attacco)+" punti vita.");
+                    break;
+                case 2:
+                    k = rand.nextInt(2);
+                    int indice = rand.nextInt(10);
+                    Weapon arma = new Weapon(arrayDeiDanni[indice],arrayDeiNomi[indice]);
+                    switch (k) {
+                        case 0:
+                            gargoyle.raccogliArma(arma);
+                            break;
+                        case 1:
+                            yeti.raccogliArma(arma);
+                            break;
+                    }
+                    break;
+                case 3:
+                    k = rand.nextInt(2);
+                    switch (k) {
+                        case 0:
+                            gargoyle.perdiArma();
+                            break;
+                        case 1:
+                            yeti.perdiArma();
+                            break;
+                    }
                     break;
             }
         }
